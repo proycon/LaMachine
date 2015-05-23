7b2a6776e7644133cef561501308c44a90194a33
Maarten van Gompel <proycon@anaproy.nl>
2015-05-23 23:06:38 +0200

    fix

--- virtualenv-bootstrap.sh
+++ virtualenv-bootstrap.sh
@@ -50,7 +50,7 @@ if [ "$1" != "noadmin" ]; then
     elif [ "$OS" == "debian" ]; then
         INSTALL="sudo apt-get install pkg-config git-core make gcc g++ autoconf-archive libtool autotools-dev libicu-dev libxml2-dev libbz2-dev zlib1g-dev libtar-dev libboost-all-dev python-dev cython python3 python-pip python3-pip cython cython3 python3-requests python-lxml python3-lxml python3-pycurl python-virtualenv python-numpy python3-numpy python-scipy python3-scipy python-matplotlib python3-matplotlib python-pandas python3-pandas python-requests python3-requests" 
     elif [ "$OS" == "redhat" ]; then
-        INSTALL="sudo yum pkgconfig git icu icu-devel libtool autoconf automake autoconf-archive make gcc xml2 xml2-devel libtar libtar-devel boost boost-devel python python-devel python3 python-lxml python3-lxml Cython zlib zlib-devel python-numpy python3-numpy scipy python3-scipy python-matplotlib python3-matplotlib python3-virtualenv python-pip python3-pip bzip2 bzip2-devel"
+        INSTALL="sudo yum install pkgconfig git icu icu-devel libtool autoconf automake autoconf-archive make gcc xml2 xml2-devel libtar libtar-devel boost boost-devel python python-devel python3 python-lxml python3-lxml Cython zlib zlib-devel python-numpy python3-numpy scipy python3-scipy python-matplotlib python3-matplotlib python3-virtualenv python-pip python3-pip bzip2 bzip2-devel"
     elif [ "$OS" == "mac" ]; then
         INSTALL="brew install python3 autoconf automake libtool autoconf-archive boost xml2 icu4c"
     else
