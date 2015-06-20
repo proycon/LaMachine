5e8f71a4803e00bacfe29fb20de59098f3e9b1eb
Maarten van Gompel <proycon@anaproy.nl>
2015-06-20 19:48:27 +0200

    added test.sh

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+RETURN=0
+
+testerror () {
+    echo "================ TEST ERROR ==============" >&2
+    echo $1 >&2
+    echo "===========================================" >&2
+    RETURN=2
+}
+
+which ucto || testerror "Ucto not found"
+which frog || testerror "Frog not found"
+which timbl || testerror "Timbl not found"
+which mbt || testerror "Mbt not found"
+which wopr || testerror "Wopr not found"
+which foliavalidator || testerror "FoLiA validator not found"
+python -c "import pynlpl" || testerror "PyNLPl not found"
+python -c "import timbl" || testerror "python-timbl not found"
+python -c "import ucto" || testerror "python-ucto not found"
+which patternmodeller || testerror "Colibri Core not found"
+python -c "import colibricore" || testerror "colibricore not found for python"
+python -c "import frog" || testerror "python-frog not found"
+
+if [ "$RETURN" != "0" ]; then
+    testerror "Test failure... One or more errors occured!"
+fi
+
+exit $RETURN
