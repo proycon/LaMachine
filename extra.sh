282824666ff2be8392baed0c14a46c1bff77b7a2
Maarten van Gompel <proycon@anaproy.nl>
2015-11-02 11:02:51 +0100

    fix

--- extra.sh
+++ extra.sh
@@ -67,7 +67,7 @@ if [ $WITHTSCAN -eq 1 ] || [ -d tscan ]; then
     cd ..
 fi
 
-if [ $WITHVALKUIL -eq 1 ] || [-d valkuil-gecco ]; then
+if [ $WITHVALKUIL -eq 1 ] || [ -d valkuil-gecco ]; then
     project="valkuil-gecco"
     echo 
     echo "--------------------------------------------------------"
