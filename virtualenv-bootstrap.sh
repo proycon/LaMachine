d5bd08ab0fdbcf2d11491f5744fa03ee07678caa
Maarten van Gompel <proycon@anaproy.nl>
2016-03-14 15:32:03 +0100

    activate_this.py got corrupted for some reason

--- virtualenv-bootstrap.sh
+++ virtualenv-bootstrap.sh
@@ -559,7 +559,7 @@ base = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if sys.platform == "win32":
     site_packages = os.path.join(base, "Lib", "site-packages")
 else:
-    site_packages = os.path.join(base, "lib", "python%s" % sys.version[:3], "site-packages")
+    site_packages = os.path.join(base, "lib", "python" + str(sys.version[:3]), "site-packages")
 prev_sys_path = list(sys.path)
 import site
 site.addsitedir(site_packages)
