b5d846b8aa61e2fd133f866bddfbbe2b8f661501
Maarten van Gompel <proycon@anaproy.nl>
2016-06-14 11:43:47 +0200

    removed LuigiNLP from LaMachine again, issue with dependency python-daemon (although it does install)

--- virtualenv-bootstrap.sh
+++ virtualenv-bootstrap.sh
@@ -776,7 +776,7 @@ fi
 PYTHONMAJOR=$(python -c "import sys; print(sys.version_info.major,end='')")
 PYTHONMINOR=$(python -c "import sys; print(sys.version_info.minor,end='')")
 
-PYTHONPROJECTS="proycon/pynlpl proycon/folia proycon/foliadocserve proycon/flat LanguageMachines/LuigiNLP"
+PYTHONPROJECTS="proycon/pynlpl proycon/folia proycon/foliadocserve proycon/flat" #LanguageMachines/LuigiNLP
 
 
 echo 
