2ff0ce2dbccf7698fda4fc8b9d121ef9739bfc70
Maarten van Gompel <proycon@anaproy.nl>
2017-02-23 21:52:48 +0100

    added PPA upload

--- build-ubuntu-ppa.sh
+++ build-ubuntu-ppa.sh
@@ -12,7 +12,7 @@ cd ubuntu-ppa
 for package in $PACKAGES; do
     echo 
     echo "--------------------------------------------------------"
-    echo "Installing/updating $package"
+    echo "Building package for $package"
     echo "--------------------------------------------------------"
     if [ ! -d $package ]; then
         git clone https://anonscm.debian.org/git/debian-science/packages/$package.git || fatalerror "Unable to clone git repo for $package"
@@ -21,8 +21,17 @@ for package in $PACKAGES; do
         cd $package
         git pull
     fi
+    echo "Previously packaged versions: " 
+    ls $package*source.changes
     sed -i 's/debhelper (>= 10)/debhelper (>= 9)/' $package/debian/control #debhelper 10 is too new for 16.04
     dch -i && debcommit -a -m "Rebuilding for ubuntu" && gbp buildpackage -S -k1A31555C
+    if [ $? -eq 0 ]; then
+        changefile=$(ls -t $package*source.changes | head -n 1)
+        if [ ! -z "$changefile" ]; then
+            echo "Uploading $changefile"
+            dput lamachine $changefile
+        fi
+    fi
     #wget -r -nc --no-parent --reject "index.html*" http://cdn-fastly.deb.debian.org/debian/pool/main/${package:0:1}/$package/ 
     #dget -ux 
 done
