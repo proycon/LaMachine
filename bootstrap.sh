8fe69cba8e79558130b43fccefbc666e8ed4adfa
Maarten van Gompel <proycon@anaproy.nl>
2017-03-03 21:28:20 +0100

    it helps if we actually install flat...

--- bootstrap.sh
+++ bootstrap.sh
@@ -431,7 +431,7 @@ done
 #echo "--------------------------------------------------------"
 #pip2 install pynlpl FoLiA-tools clam || error "Installation of one or more Python 2 packages failed !!"
 
-PYPIPROJECTS="pynlpl FoLiA-tools python-ucto foliadocserve clam python3-timbl python-frog colibricore"
+PYPIPROJECTS="pynlpl FoLiA-tools python-ucto foliadocserve clam python3-timbl python-frog colibricore FoLiA-Linguistic-Annotation-Tool"
 
 if [ ! -z "$PYPIPROJECTS" ]; then
     if [ -z "$VERSIONFILE" ]; then
