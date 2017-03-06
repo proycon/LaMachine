50e508f4de1cb93cd0fc6e6c4ed86e3f7a66162a
Maarten van Gompel <proycon@anaproy.nl>
2017-03-06 13:11:37 +0100

    do not use git in foliadocserve

--- startwebservices.sh
+++ startwebservices.sh
@@ -15,7 +15,7 @@ CLAMFORCEURL=http://127.0.0.1:$PORT/timbl/ uwsgi --plugins python --socket :3033
 CLAMFORCEURL=http://127.0.0.1:$PORT/colibricore/ uwsgi --plugins python --socket :3034 --chdir /usr/src/_clamservices/wsgi --mount /colibricore=/usr/src/_clamservices/wsgi/colibricore.wsgi  --manage-script-name --master --processes 1 --threads 2 &
 pkill -9 foliadocserve
 cd #change path so the log file can be written
-foliadocserve -d /var/flat.docroot --git --expirationtime 120 -p 3030 &
+foliadocserve -d /var/flat.docroot --expirationtime 120 -p 3030 &
 cd -
 export PYTHONPATH=/usr/src/LaMachine
 uwsgi --plugins python --socket :3035 --chdir /usr/src/LaMachine --mount /flat=/usr/src/LaMachine/flat.wsgi  --manage-script-name --master --processes 1 --threads 2 &
