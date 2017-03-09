bada0066afb982436e1ace447ba86f75c1cd98f5
Maarten van Gompel <proycon@anaproy.nl>
2017-03-09 21:53:07 +0100

    adapted to allow specifying a full custom base URL instead of just a port #25

--- startwebservices.sh
+++ startwebservices.sh
@@ -2,17 +2,17 @@
 
 #This script should be launched only from within the LaMachine VM or Docker app
 if [ ! -z "$1" ]; then
-    PORT=$1
+    URL=$1
 else
-    PORT=8080
+    URL="http://127.0.0.1:8080"
 fi
 
 
 
-CLAMFORCEURL=http://127.0.0.1:$PORT/ucto/ uwsgi --plugins python --socket :3031 --chdir /usr/src/_clamservices/wsgi --mount /ucto=/usr/src/_clamservices/wsgi/ucto.wsgi --manage-script-name --master --processes 1 --threads 2 &
-CLAMFORCEURL=http://127.0.0.1:$PORT/frog/ uwsgi --plugins python --socket :3032 --chdir /usr/src/_clamservices/wsgi --mount /frog=/usr/src/_clamservices/wsgi/frog.wsgi  --manage-script-name --master --processes 1 --threads 2 &
-CLAMFORCEURL=http://127.0.0.1:$PORT/timbl/ uwsgi --plugins python --socket :3033 --chdir /usr/src/_clamservices/wsgi --mount /timbl=/usr/src/_clamservices/wsgi/timbl.wsgi  --manage-script-name --master --processes 1 --threads 2 &
-CLAMFORCEURL=http://127.0.0.1:$PORT/colibricore/ uwsgi --plugins python --socket :3034 --chdir /usr/src/_clamservices/wsgi --mount /colibricore=/usr/src/_clamservices/wsgi/colibricore.wsgi  --manage-script-name --master --processes 1 --threads 2 &
+CLAMFORCEURL=$BASEURL/ucto/ uwsgi --plugins python --socket :3031 --chdir /usr/src/_clamservices/wsgi --mount /ucto=/usr/src/_clamservices/wsgi/ucto.wsgi --manage-script-name --master --processes 1 --threads 2 &
+CLAMFORCEURL=$BASEURL/frog/ uwsgi --plugins python --socket :3032 --chdir /usr/src/_clamservices/wsgi --mount /frog=/usr/src/_clamservices/wsgi/frog.wsgi  --manage-script-name --master --processes 1 --threads 2 &
+CLAMFORCEURL=$BASEURL/timbl/ uwsgi --plugins python --socket :3033 --chdir /usr/src/_clamservices/wsgi --mount /timbl=/usr/src/_clamservices/wsgi/timbl.wsgi  --manage-script-name --master --processes 1 --threads 2 &
+CLAMFORCEURL=$BASEURL/colibricore/ uwsgi --plugins python --socket :3034 --chdir /usr/src/_clamservices/wsgi --mount /colibricore=/usr/src/_clamservices/wsgi/colibricore.wsgi  --manage-script-name --master --processes 1 --threads 2 &
 pkill -9 foliadocserve
 cd #change path so the log file can be written
 foliadocserve -d /var/flat.docroot --expirationtime 120 -p 3030 &
