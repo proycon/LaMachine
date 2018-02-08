46b6b34e4e73d91c856a59f574be88148e186ed0
Maarten van Gompel <proycon@anaproy.nl>
2018-02-08 13:52:46 +0100

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #======================================
-# LaMachine
+# LaMachine v2
 #  by Maarten van Gompel
 #  Centre for Language Studies
 #  Radboud University Nijmegen
@@ -18,9 +18,14 @@ echo "           / / /|	        Radboud University Nijmegen "
 echo "====================================================================="
 echo
 
+
+BASEDIR=$(pwd)
+
+
 ####################################################
 #               Platform Detection
 ####################################################
+NEED=() #list of needed packages
 ARCH=$(which pacman 2> /dev/null)
 DEBIAN=$(which apt 2> /dev/null)
 REDHAT=$(which yum 2> /dev/null)
@@ -33,15 +38,6 @@ elif [ -f "$REDHAT" ]; then
 else
     if [ ${OSTYPE//[0-9.]/} = "darwin" ]; then
         OS="mac"
-        which brew 2>/dev/null
-        if [ $? -ne 0 ]; then
-            NEED+=("brew")
-        fi
-        if brew info brew-cask | grep "brew-cask" >/dev/null 2>&1 ; then
-            echo
-        else
-            NEED+=("brew-cask")
-        fi
 
     else
         OS="unknown"
@@ -63,17 +59,31 @@ echo "Detected distribution ID: $DISTRIB_ID"
 echo "Detected distribution release: $DISTRIB_RELEASE"
 echo
 
-NEED=() #list of needed packages
 
-#get source directory of current script and set as default LaMachine SOURCEDIR
-SOURCEDIR="${BASH_SOURCE[0]}"
-while [ -h "$SOURCEDIR" ]; do # resolve $SOURCEDIR until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCEDIR" )" && pwd )"
-  SOURCEDIR="$(readlink "$SOURCEDIR")"
-  [[ $SOURCEDIR != /* ]] && SOURCEDIR="$DIR/$SOURCEDIR" # if $SOURCEDIR was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-cd $SOURCEDIR
-
+echo "Looking for dependencies..."
+if ! which git; then
+    NEED+=("git")
+fi
+if ! which virtualenv; then
+    NEED+=("virtualenv")
+fi
+if [ ! -z "$EDITOR" ]; then
+    if which nano; then
+        EDITOR=nano
+    else
+        EDITOR=vi
+    fi
+fi
+if [[ "$OS" == "mac" ]]; then
+    if ! which brew; then
+        NEED+=("brew")
+    fi
+    if brew info brew-cask | grep "brew-cask" >/dev/null 2>&1 ; then
+        echo "brew-cask found"
+    else
+        NEED+=("brew-cask")
+    fi
+fi
 
 while [[ $# -gt 0 ]]; do
     key="$1"
@@ -98,13 +108,23 @@ while [[ $# -gt 0 ]]; do
         shift # past argument
         shift # past value
         ;;
+        -b|--branch) #branch of LaMachine git
+        BRANCH="$2"
+        shift # past argument
+        shift # past value
+        ;;
+        --gitrepo) #git repository of LaMachine
+        GITREPO="$2"
+        shift # past argument
+        shift # past value
+        ;;
         --env) #conda or virtualenv
         LOCALENV_TYPE="$2"
         shift # past argument
         shift # past value
         ;;
         --source) #LaMachine source path
-        SOURCEDIR="$2"
+        BASEDIR="$2"
         shift # past argument
         shift # past value
         ;;
@@ -119,18 +139,6 @@ echo "Welcome to the LaMachine Installation tool, we will ask some questions how
 echo "you want your LaMachine to be installed."
 echo
 
-which wget 2>/dev/null
-if [ $? -eq 0 ]; then
-    DL=wget
-else
-    which curl 2>/dev/null
-    if [ $? -eq 0 ]; then
-        DL=curl
-    else
-        echo "No download tool was found on your system! Please install wget or curl first...">&2
-        exit 2
-    fi
-fi
 
 if [ -z "$FLAVOUR" ]; then
     while true; do
@@ -206,22 +214,30 @@ if [ -z "$VERSION" ]; then
     done
 fi
 
+if [ -z "$BRANCH" ]; then
+    if [[ "$VERSION" == "development" ]]; then
+        BRANCH="lamachine2" #TODO: change to develop
+    else
+        BRANCH="lamachine2" #TODO: change to master
+    fi
+fi
+if [ -z "$GITREPO" ]; then
+    GITREPO="https://github.com/proycon/LaMachine"
+fi
+
 if [ "$FLAVOUR" == "vagrant" ]; then
-    which vagrant 2>/dev/null
-    if [ $? -eq 1 ]; then
+    echo "Looking for vagrant..."
+    if ! which vagrant; then
         NEED+=("vagrant")
     fi
 fi
 
 if [ "$FLAVOUR" == "docker" ]; then
-    which docker 2>/dev/null
-    NEED=()
-    if [ $? -eq 1 ]; then
-        NEED+=("docker")
-    fi
-    which ansible 2>/dev/null
-    if [ $? -eq 1 ]; then
-        NEED+=("ansible")
+    echo "Looking for docker..."
+    if ! which docker; then
+        if ! which docker.io; then
+            NEED+=("docker")
+        fi
     fi
 fi
 
@@ -296,24 +312,78 @@ for package in $NEED; do
     elif [ "$package" = "brew-cask" ]; then
         echo "Installing brew-cask"
         brew install brew-cask
+    elif [ "$package" = "git" ]; then
+        if [ "$OS" = "debian" ]; then
+            cmd="sudo apt install git-core"
+        elif [ "$OS" = "redhat" ]; then
+            cmd="sudo yum install git"
+        elif [ "$OS" = "arch" ]; then
+            cmd="sudo pacman -Sy git"
+        elif [ "$OS" = "mac" ]; then
+            cmd="brew install git"
+        else
+            cmd=""
+        fi
+        echo "Git is required for LaMachine but not installed yet. Install now?"
+        if [ ! -z "$cmd" ]; then
+            while true; do
+                echo -n "Run: $cmd ? [yn]"
+                read yn
+                case $yn in
+                    [Yy]* ) $cmd; break;;
+                    [Nn]* ) echo "Please install git manually" && echo " .. press ENTER when done or CTRL-C to abort..." && read; break;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        else
+            echo "No automated installation possible on your OS."
+            echo "Please install git manually" && echo " .. press ENTER when done or CTRL-C to abort..." && read
+        fi
+    elif [ "$package" = "virtualenv" ]; then
+        if [ "$OS" = "debian" ]; then
+            cmd="sudo apt install python-virtualenv"
+        elif [ "$OS" = "redhat" ]; then
+            cmd="sudo yum install python-virtualenv"
+        elif [ "$OS" = "arch" ]; then
+            cmd="sudo pacman -Sy python-virtualenv"
+        elif [ "$OS" = "mac" ]; then
+            cmd="sudo easy_install pip && sudo pip install virtualenv"
+        else
+            cmd=""
+        fi
+        echo "Virtualenv is required for LaMachine but not installed yet. Install now?"
+        if [ ! -z "$cmd" ]; then
+            while true; do
+                echo -n "Run: $cmd ? [yn]"
+                read yn
+                case $yn in
+                    [Yy]* ) $cmd; break;;
+                    [Nn]* ) echo "Please install git manually" && echo " .. press ENTER when done or CTRL-C to abort..." && read; break;;
+                    * ) echo "Please answer yes or no.";;
+                esac
+            done
+        else
+            echo "No automated installation possible on your OS."
+            echo "Please install virtualenv manually" && echo " .. press ENTER when done or CTRL-C to abort..." && read
+        fi
     fi
 done
 
 if [ -z "$NAME" ]; then
     echo "Your LaMachine installation is identified by a name (used as hostname, local env name, VM name) etc.."
-    echo -n "Enter a name for your LaMachine installation: "
+    echo -n "Enter a name for your LaMachine installation (no spaces!): "
     read NAME
     NAME="${NAME%\\n}"
 fi
 
-CONFIGFILE="host-vars/$NAME.yml"
+CONFIGFILE="$BASEDIR/host-vars/lamachine-$NAME.yml"
+INSTALLFILE="$BASEDIR/install-$NAME.yml"
 
 if [ ! -f "$CONFIGFILE" ]; then
     echo "---
 hostname: \"$NAME\" #(for VM or docker, doesn't change hostname on existing systems)
-env_name: \"$NAME\" #(for local user environment)
+env_name: \"lamachine-$NAME\" #(for local user environment)
 version: \"$VERSION\" #stable, development or custom
-lamachine_path: \"$SOURCEDIR\" #LaMachine source path (on host machine)
 localenv_type: \"$LOCALENV_TYPE\" #Local environment type (conda or virtualenv), not used when prefer_global is true
 " > $CONFIGFILE
     if [ $PREFER_GLOBAL -eq 1 ]; then
@@ -338,21 +408,68 @@ localenv_type: \"$LOCALENV_TYPE\" #Local environment type (conda or virtualenv),
 echo "webserver: true #include a webserver
 port: 80 #webserver port (for VM or docker)
 mapped_port: 8080 #mapped webserver port on host system (for VM or docker)
-#Adapt install.yml to select the packages you want to install" >> $CONFIGFILE
+" >> $CONFIGFILE
+
+echo "Opening configuration file $CONFIGFILE in editor for final configuration..."
+sleep 3
+if ! "$EDITOR" "$CONFIGFILE"; then
+    exit 2
+fi
 fi
 
 
+cd $BASEDIR
+if [ -d .git ]; then
+    #we are in a LaMachine git repository already
+    SOURCEDIR=$BASEDIR
+fi
+
+if [ ! -d lamachine-controller ]; then
+    echo "Setting up control environment..."
+    virtualenv --python=python2.7 lamachine.control.env
+    source lamachine.control.env/bin/activate
+    pip install ansible
+    cd lamachine-controller
+else
+    echo "Reusing existing control environment..."
+    source lamachine-controller/bin/activate
+fi
+
+if [ -z "$SOURCEDIR" ]; then
+    echo "Cloning LaMachine git repo ($GITREPO $BRANCH)..."
+    git clone $GITREPO -b $BRANCH LaMachine
+    SOURCEDIR=$BASEDIR/lamachine-controller/LaMachine
+    cd $SOURCEDIR
+else
+    echo "Updating LaMachine git..."
+    cd $SOURCEDIR
+    if [ "$SOURCEDIR" != "$BASEDIR" ]; then
+        git checkout $BRANCH #only switch branches if we're not already in a git repo the user cloned himself
+    fi
+    git pull #make sure we're up to date
+fi
+ln -s $CONFIGFILE $SOURCEDIR/host-vars/$(basename $CONFIGFILE)
+if [ ! -f $INSTALLFILE ]; then
+    cp $SOURCEDIR/install.yml $INSTALLFILE
+fi
+echo "Opening installation file $INSTALLFILE in editor for selection of packages to install..."
+sleep 3
+if ! "$EDITOR" "$INSTALLFILE"; then
+    exit 2
+fi
+ln -s $INSTALLFILE $SOURCEDIR/$(basename $INSTALLFILE)
 
 if [[ "$FLAVOUR" == "vagrant" ]]; then
-    echo "Building VM"
-    vagrant up
-else
-    if [ ! -d lamachine.control.env ]; then
-        echo "Setting up control environment..."
-        virtualenv --python=python2.7 lamachine.control.env
-        source lamachine.control.env/bin/activate
-        pip install ansible
+    echo "Preparing vagrant..."
+    if [ ! -f $SOURCEDIR/Vagrantfile.$NAME ]; then
+        cp $SOURCEDIR/Vagrantfile $SOURCEDIR/Vagrantfile.$NAME
+        sed -i s/lamachine-vm/$NAME/g $SOURCEDIR/Vagrantfile.$NAME
     fi
+    echo "Running vagrant..."
+    VAGRANT_CWD=$SOURCEDIR  VAGRANT_VAGRANTFILE=Vagrantfile.$NAME vagrant up
+    echo -e "#!bin/bash\nexport VAGRANT_CWD=$SOURCEDIR VAGRANT_VAGRANTFILE=Vagrantfile.$NAME\nvagrant up\nvagrant ssh\nvagrant halt" > $BASEDIR/lamachine-$NAME.activate
+    chmod a+x $BASEDIR/lamachine-$NAME.activate
+elif [[ "$FLAVOUR" == "env" ]]; then
+    echo "TODO"
 fi
-
-
+echo "All done!"
