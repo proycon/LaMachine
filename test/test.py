7dd346210d27da9e50d5ece8f9b23a258dc0783e
Maarten van Gompel <proycon@anaproy.nl>
2018-03-09 21:41:54 +0100

    expanded test framework #54

--- test/test.py
+++ test/test.py
@@ -1,27 +1,40 @@
 #!/usr/bin/env python3
 
+import sys
 import os
 
 buildmatrix = [
     {
-        "name": 'lmtest-stable-vm-debian',
+        "name": 'lmtest-stable-vm',
         "flavour": 'vagrant',
         "version": 'stable'
     },
     {
-        "name": 'lmtest-dev-vm-debian',
+        "name": 'lmtest-dev-vm',
         "flavour": 'vagrant',
         "version": 'development'
     },
     {
-        "name": 'lmtest-stable-docker-debian',
+        "name": 'lmtest-stable-docker',
         "flavour": 'docker',
         "version": 'stable'
     },
     {
-        "name": 'lmtest-stable-docker-debian',
+        "name": 'lmtest-dev-docker',
         "flavour": 'docker',
         "version": 'development'
+    },
+    {
+        "name": 'lmtest-stable-vm-centos7',
+        "flavour": 'vagrant',
+        "version": 'stable',
+        "vagrantbox": "centos/7"
+    },
+    {
+        "name": 'lmtest-stable-vm-ubuntu1604',
+        "flavour": 'vagrant',
+        "version": 'stable',
+        "vagrantbox": "ubuntu/xenial64"
     },
 ]
 
@@ -29,6 +42,7 @@ buildmatrix = [
 
 success = {}
 for build in buildmatrix:
+    print("Building " + build['name'] + " ...", file=sys.stderr)
     args = []
     for key, value in build.items():
         args.append("--" + key + " " + value)
