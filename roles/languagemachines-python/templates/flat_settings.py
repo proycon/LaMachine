accfae515209e1f73925cad7145e6374e4ba97f3
Maarten van Gompel <proycon@anaproy.nl>
2018-03-24 12:56:33 +0100

    configuration update for FLAT #49

--- roles/languagemachines-python/templates/flat_settings.py
+++ roles/languagemachines-python/templates/flat_settings.py
@@ -276,6 +276,8 @@ MANAGERS = ADMINS
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', "{{hostname}}"]
 
+FORCE_SCRIPT_NAME = "/flat" #FLAT runs on the flat/ url prefix rather than the root
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
