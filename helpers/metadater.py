fca5686757a924c7950614b45987548251c741cc
Maarten van Gompel <proycon@anaproy.nl>
2018-04-12 13:00:12 +0200

    further brainstorm/prototyping #74

--- helpers/metadater.py
+++ helpers/metadater.py
@@ -1,11 +1,32 @@
 #!/usr/bin/env python3
 
+#This script converts software metadata
+#from different sources into a generic form.
+#Attempting to reuse as much as possible from ADMS.SW and DOAP:
+#https://joinup.ec.europa.eu/svn/adms_foss/adms_sw_v1.00/adms_sw_v1.00.htm
+
+
 import argparse
 import subprocess
 import yaml
 
-#Attempting to reuse as much as possible from ADMS.SW and DOAP:
-#https://joinup.ec.europa.eu/svn/adms_foss/adms_sw_v1.00/adms_sw_v1.00.htm
+properties = {
+    "doap:name": "The name of the software",
+    "doap:revision": "The full version number of the software",
+    "doap:description": "A brief description of what the software does",
+    "doap:homepage": "The homepage of the software",
+    "doap:developer": "The author of the software (can be used multiple times!)",
+    "dcterms:license": "The software license",
+    "lamachine:dependency": "A dependency",
+}
+
+dep_properties = {
+    "doap:name": "The name of the software",
+    "lamachine:externalDependency": "Boolean, external dependencies are dependencies in a different software ecosystem. Internal dependencies are in the same ecosystem (e.g. Python/PyPI, Java/Maven, Perl/CPAN)",
+    "lamachine:minimumVersion": "Minimum version",
+    "lamachine:maximumVersion": "Maximum version",
+}
+
 
 pip_mapping = {
     "Name": "doap:name",
@@ -15,9 +36,13 @@ pip_mapping = {
     "Author": "doap:developer",
     "Licence": "dcterms:license",
     "Requires": "lamachine:dependencies",
+    "Location": "lamachine:installLocation",
 }
 pip_classifier_mapping = {
     "Operating System": "schema:operatingSystem",
+    "Development Status": "admssw:status",
+    "Topic": "rad:theme",
+    "Indented Audience": "admssw:intendedAudience",
 }
 
 def parsepip(lines):
@@ -29,12 +54,17 @@ def parsepip(lines):
         elif line.strip() = "Entry-points:":
             section = "interfaces"
         elif section == "classifiers":
+            fields = line.strip().split('::')
+            if fields[0] in mapping:
+                data[pip_classifier_mapping[fields[0]].split(':',1)[1:]] = "
+
+
         elif section == "interfaces":
 
         else:
             key, value = line.split(':',1)
             if key in mapping:
-                data[mapping[key].split(':',1)[1:]] = value
+                data[pip_mapping[key].split(':',1)[1:]] = value
             elif key == "Author-email":
                 data['developer'] += " <" + value + ">"
 
