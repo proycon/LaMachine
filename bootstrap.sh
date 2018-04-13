babde48399b58ea392cfcbf7de8bef3f85f70c2b
Maarten van Gompel <proycon@anaproy.nl>
2018-04-13 10:48:07 +0200

    added stackoverflow URL for more context

--- bootstrap.sh
+++ bootstrap.sh
@@ -112,7 +112,7 @@ if which python; then
     echo "Checking sanity of your Python installation..."
     python -c "from __future__ import print_function; import sys; print(sys.version)" | grep -i anaconda
     if [ $? -eq 0 ]; then
-        fatalerror "Conflict error: The default Python on this system is managed by Anaconda, this is incompatible with LaMachine. Ensure the Python found in your \$PATH corresponds to a regular version as supplied with your OS, editing the order of your \$PATH in ~/.bashrc or ~/.bash_profile should be sufficient to solve this without completely uninstalling anaconda."
+        fatalerror "Conflict error: The default Python on this system is managed by Anaconda, this is incompatible with LaMachine. Ensure the Python found in your \$PATH corresponds to a regular version as supplied with your OS, editing the order of your \$PATH in ~/.bashrc or ~/.bash_profile should be sufficient to solve this without completely uninstalling anaconda. See also https://stackoverflow.com/a/37377981/3311445"
     fi
 else
     fatalerror "No Python found! However, python should be available by default on all supported platforms; please install it yourself through your package manager (and ensure it is in your \$PATH)"
