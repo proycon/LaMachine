03c6f9373acd14244bcf3789b19707a341a5f7c4
Maarten van Gompel <proycon@anaproy.nl>
2018-04-25 19:44:43 +0200

    version bump

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ boldblue=${bold}$(tput setaf 4) #  blue
 normal=$(tput sgr0)
 
 echo "${bold}=====================================================================${normal}"
-echo "           ,              ${bold}LaMachine v2.1.4${normal} - NLP Software distribution"
+echo "           ,              ${bold}LaMachine v2.1.5${normal} - NLP Software distribution"
 echo "          ~)                     (http://proycon.github.io/LaMachine)"
 echo "           (----í         Language Machines research group"
 echo "            /| |\         Centre of Language and Speech Technology"
