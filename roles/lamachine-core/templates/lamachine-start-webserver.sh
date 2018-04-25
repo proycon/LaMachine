1cb2ec87df04489e24f83270673c54c2ab57d84c
Maarten van Gompel <proycon@anaproy.nl>
2018-04-25 19:06:29 +0200

    attempted fix for broken webserver script when there is no systemd (e.g. docker) #80

--- roles/lamachine-core/templates/lamachine-start-webserver.sh
+++ roles/lamachine-core/templates/lamachine-start-webserver.sh
@@ -6,19 +6,35 @@
 
 {% if locality == "global" and root %}
 #global flavour
+if which systemctl >/dev/null 2>/dev/null; then
+    HAVE_SYSTEMCTL=1
+else
+    HAVE_SYSTEMCTL=0 #If there is no systemd, assume init V and 'service' command, this is relevant also in most docker containers where systemd makes less sense
+fi
 
 #kill and restart uwsgi emperor
 {% if ansible_distribution|lower == "debian" or ansible_distribution|lower == "ubuntu" or ansible_distribution|lower == "linux mint" %}
- sudo systemctl enable uwsgi-emperor
- sudo systemctl start uwsgi-emperor
+if [ $HAVE_SYSTEMCTL -eq 1 ]; then
+     sudo systemctl enable uwsgi-emperor
+     sudo systemctl start uwsgi-emperor
+else
+     sudo service uwsgi-emperor start
+fi
 {% else %}
- sudo uwsgi --ini "{{lm_prefix}}/etc/uwsgi-emperor/emperor.ini" --die-on-term &
+     sudo uwsgi --ini "{{lm_prefix}}/etc/uwsgi-emperor/emperor.ini" --die-on-term &
 {% endif %}
 
 {% if  webservertype == "nginx" %}
- sudo systemctl enable nginx
- sudo systemctl start nginx
+if [ $HAVE_SYSTEMCTL -eq 1 ]; then
+     sudo systemctl enable nginx
+     sudo systemctl start nginx
+else
+     sudo service nginx start
+fi
+{% else %}
+    echo "You are using a non-default webservertype, unable to manage webserver for you...">&2
 {% endif %}
+
 
 
 {% else %}
@@ -27,8 +43,15 @@
 uwsgi --ini "{{lm_prefix}}/etc/uwsgi-emperor/emperor.ini" --die-on-term &
 
 {% if webservertype == "nginx" %}
-nginx -c "{{lm_prefix}}/etc/nginx.conf" -p "{{lm_prefix}}/share/nginx"  -g "pid {{lm_prefix}}/var/run/nginx.pid; worker_processes 2;"
+    nginx -c "{{lm_prefix}}/etc/nginx.conf" -p "{{lm_prefix}}/share/nginx"  -g "pid {{lm_prefix}}/var/run/nginx.pid; worker_processes 2;"
+{% else %}
+    echo "You are using a non-default webservertype, unable to manage webserver for you...">&2
 {% endif %}
 
 
 {% endif %}
+
+if [ "$1" = "-f" ]; then
+    #run in foreground/keep running (nginx error log)
+    tail -F "{{lm_prefix}}/var/log/nginx/error.log"
+fi
--- roles/lamachine-core/templates/lamachine-stop-webserver.sh
+++ roles/lamachine-core/templates/lamachine-stop-webserver.sh
@@ -4,16 +4,29 @@
 
 {% if locality == "global" and root %}
 #global flavour
+if which systemctl >/dev/null 2>/dev/null; then
+    HAVE_SYSTEMCTL=1
+else
+    HAVE_SYSTEMCTL=0 #If there is no systemd, assume init V, this is relevant also in most docker containers where systemd makes less sense
+fi
 
 #kill and restart uwsgi emperor
 {% if ansible_distribution|lower == "debian" or ansible_distribution|lower == "ubuntu" or ansible_distribution|lower == "linux mint" %}
- sudo systemctl stop uwsgi-emperor
+if [ $HAVE_SYSTEMCTL -eq 1 ]; then
+     sudo systemctl stop uwsgi-emperor
+else
+     sudo service uwsgi-emperor stop
+fi
 {% else %}
  sudo killall -w uwsgi
 {% endif %}
 
 {% if  webservertype == "nginx" %}
- sudo systemctl stop nginx
+if [ $HAVE_SYSTEMCTL -eq 1 ]; then
+     sudo systemctl stop nginx
+else
+     sudo service nginx stop
+fi
 {% endif %}
 
 
