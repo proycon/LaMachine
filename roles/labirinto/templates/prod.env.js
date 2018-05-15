c4d82bd502c355a515681666bce87cb18eacbfa2
Maarten van Gompel <proycon@anaproy.nl>
2018-05-15 21:06:31 +0200

    deploying labirinto portal

---
+++ roles/labirinto/templates/prod.env.js
@@ -0,0 +1,12 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  TITLE: '"LaMachine Portal"',
+  DESCRIPTION: '"This portal provides an overview of available Natural Language Processing software in this installation of LaMachine, use the filters above to broaden or narrow the selection."',
+  VERSION: '"0.1.0"',
+  REGISTRY_URL: '"{{lm_base_url}}/metadata.json"',
+  LOGO_RIGHT: "true",
+  LOGO_LEFT: "true",
+  ORGANIZATIONS: [],
+  DOMAINS: ["{{hostname}}"],
+}
