4d9b261afe6e493265859b6d73db57272159c016
Maarten van Gompel <proycon@anaproy.nl>
2018-05-16 16:40:47 +0200

    use configurable variables to set labirinto parameters

--- roles/labirinto/templates/prod.env.js
+++ roles/labirinto/templates/prod.env.js
@@ -1,12 +1,12 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  TITLE: '"LaMachine Portal"',
-  DESCRIPTION: '"This portal provides an overview of available Natural Language Processing software in this installation of LaMachine, use the filters above to broaden or narrow the selection."',
+  TITLE: '"{{portal_title}}"',
+  DESCRIPTION: '"{{portal_description}}"',
   VERSION: '"0.1.0"',
-  REGISTRY_URL: '"{{lm_base_url}}/metadata.json"',
+  REGISTRY_URL: '"{{portal_registry_url}}"',
   LOGO_RIGHT: "true",
   LOGO_LEFT: "true",
-  ORGANIZATIONS: '[]',
-  DOMAINS: '["{{hostname}}"]',
+  ORGANIZATIONS: '{{portal_organizations|to_json}}',
+  DOMAINS: '{{portal_domains|to_json}}',
 }
