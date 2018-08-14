b3c478cff1685b674936d7ca368944e6577af2ab
Maarten van Gompel <proycon@anaproy.nl>
2018-08-14 17:18:51 +0200

    added but disabled jupyterlab git extension (doesn't seem to want to build yet, leaving it here to solve in the future) #108

--- roles/jupyter/templates/jupyter_notebook_config.py
+++ roles/jupyter/templates/jupyter_notebook_config.py
@@ -4,3 +4,4 @@ c.NotebookApp.base_url = '/lab'
 c.NotebookApp.password = '{{lab_password_sha1}}'
 c.NotebookApp.port = 9888
 c.NotebookApp.trust_xheaders = True #necessary if reverse proxy handles SSL
+#c.NotebookApp.nbserver_extensions = {"jupyterlab_git": True}
