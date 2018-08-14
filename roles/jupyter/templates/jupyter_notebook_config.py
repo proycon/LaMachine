8357ca5ddbe531a9ba76159567a01b15fd1db06d
Maarten van Gompel <proycon@anaproy.nl>
2018-08-14 16:28:05 +0200

    fix

--- roles/jupyter/templates/jupyter_notebook_config.py
+++ roles/jupyter/templates/jupyter_notebook_config.py
@@ -1,4 +1,4 @@
 c.NotebookApp.allow_origin = '{{lab_allow_origin}}'
 c.NotebookApp.base_url = '/lab'
-c.NotebookApp.password = 'sha1:{{lab_password_sha1}}'
+c.NotebookApp.password = '{{lab_password_sha1}}'
 c.NotebookApp.port = 9888
