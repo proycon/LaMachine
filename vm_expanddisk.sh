80fb8f84e7d95335e7a72c76170d574fac98d67f
Maarten van Gompel <proycon@anaproy.nl>
2018-10-03 23:41:19 +0200

    removed

--- vm_expanddisk.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#never run this yourself! only to be run in a VM context
-pvcreate /dev/sdb
-vgextend VolGroup /dev/sdb
-lvextend /dev/VolGroup/lv_root /dev/sdb
-resize2fs /dev/VolGroup/lv_root
