cc46deaca6fd68364faa57e795c7080fe8ef2cfd
Maarten van Gompel <proycon@anaproy.nl>
2021-03-05 16:50:31 +0100

    jupyter notebook configuration fix

--- roles/jupyter/templates/jupyter_notebook_config.py
+++ roles/jupyter/templates/jupyter_notebook_config.py
@@ -1,6 +1,4 @@
-c.NotebookApp.password_required = True
 c.NotebookApp.allow_origin = '{{lab_allow_origin}}'
-c.NotebookApp.trust_xheaders = True #necessary if reverse proxy handles SSL
 c.NotebookApp.notebook_dir = '{{www_data_path}}/notebooks'
 c.NotebookApp.terminado_settings = { 'shell_command': ['/bin/bash','-l'] }
 #c.NotebookApp.nbserver_extensions = {"jupyterlab_git": True}
