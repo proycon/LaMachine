39cb66ccb7be5b992a307273a04ed26301ed0ca7
Maarten van Gompel <proycon@anaproy.nl>
2021-03-05 16:55:11 +0100

    notebook fixes

--- roles/jupyter/templates/jupyter_notebook_config.py
+++ roles/jupyter/templates/jupyter_notebook_config.py
@@ -1,4 +1,3 @@
 c.NotebookApp.allow_origin = '{{lab_allow_origin}}'
-c.NotebookApp.notebook_dir = '{{www_data_path}}/notebooks'
 c.NotebookApp.terminado_settings = { 'shell_command': ['/bin/bash','-l'] }
 #c.NotebookApp.nbserver_extensions = {"jupyterlab_git": True}
