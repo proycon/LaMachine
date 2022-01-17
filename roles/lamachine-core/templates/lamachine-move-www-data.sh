5e3634d8020355d2cc6a078a4ab90eb2ba0ab40e
Maarten van Gompel <proycon@anaproy.nl>
2022-01-18 00:12:08 +0100

    Refactored www-data dir handling. Split www_data_path into a final www_data_path (on a shared volume if shared_www_data is true) and a www_data_staging_path (typically $LM_PREFIX/var/www-data). Such staging is needed for docker and move occurs on first server start, but for vagrant the staging can be directly into the target location.

--- roles/lamachine-core/templates/lamachine-move-www-data.sh
+++ roles/lamachine-core/templates/lamachine-move-www-data.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-if [ "{{www_data_path}}" != "{{lm_prefix}}/var/www-data" ]; then
-  rm -rf "{{ lm_prefix }}/var/www-data.bak" >/dev/null 2>/dev/null
-  if [ ! -d "{{www_data_path}}" ] && [ -d "{{ lm_prefix }}/var/www-data" ]; then
-    echo "Moving www-data to external data volume... ({{data_path}}/www-data)">&2
-    mv "{{lm_prefix}}/var/www-data" "{{www_data_path}}" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
-    ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
-  elif [ -d "{{www_data_path}}" ] && [ ! -L "{{ lm_prefix }}/var/www-data" ]; then
-    echo "External data volume already contains www-data! Refusing to overwrite, just linking to existing data...">&2
-    mv "{{lm_prefix}}/www-data" "{{www_data_path}}" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
-    ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
+if [ "{{www_data_path}}" != "{{www_data_staging_path}}" ]; then
+  rm -rf "{{www_data_staging_path}}.bak" >/dev/null 2>/dev/null
+  if [ ! -d "{{www_data_path}}" ] && [ -d "{{www_data_staging_path}}" ]; then
+      echo "Moving www-data to final location (e.g. external data volume, {{www_data_path}})">&2
+    mv "{{www_data_staging_path}}" "{{www_data_path}}" 2>/dev/null || mv "{{www_data_staging_path}}" "{{www_data_staging_path}}.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    ln -s "{{www_data_path}}" "{{www_data_staging_path}}"
+  elif [ -d "{{www_data_path}}" ] && [ ! -L "{{www_data_staging_path}}" ]; then
+    echo "Final location {{www_data_path}} already contains has www-data! Refusing to overwrite, just linking to existing data...">&2
+    mv "{{www_data_staging_path}}" "{{www_data_path}}" 2>/dev/null || mv "{{www_data_staging_path}}" "{{www_data_staging_path}}.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    ln -s "{{www_data_path}}" "{{www_data_staging_path}}"
   else
-    echo "www-data already exists in on external volume and link was already established.. all ok">&2
+      echo "www-data already exists in final location {{www_data_path}} (e.g external volume) and link was already established.. all ok">&2
   fi
+else
+    echo "www-data path is ok (staging location == final location)">&2
 fi
