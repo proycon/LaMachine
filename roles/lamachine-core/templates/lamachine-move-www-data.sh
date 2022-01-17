5acc95fc0b86d8faeb7ea404eaac54ea98052d94
Maarten van Gompel <proycon@anaproy.nl>
2022-01-17 09:30:56 +0100

    cleanup

--- roles/lamachine-core/templates/lamachine-move-www-data.sh
+++ roles/lamachine-core/templates/lamachine-move-www-data.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if [ -d "{{data_path}}" ]; then
-  rm -rf {{ www_data_path }}.bak >/dev/null 2>/dev/null
+  rm -rf "{{ www_data_path }}.bak" >/dev/null 2>/dev/null
   if [ "{{www_data_path}}" != "{{data_path}}/www-data" ]; then
       if [ ! -e "{{data_path}}/www-data" ]; then
         echo "Moving www-data to external data volume... ({{data_path}}/www-data)">&2
-        mv {{www_data_path}} {{data_path}}/www-data 2>/dev/null || mv {{www_data_path}} {{www_data_path}}.bak 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+        mv "{{www_data_path}}" "{{data_path}}/www-data" 2>/dev/null || mv "{{www_data_path}}" "{{www_data_path}}.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
         ln -sf {{data_path}}/www-data {{www_data_path}}
       else
         echo "External data volume already contains www-data! Refusing to overwrite, just linking to existing data...">&2
-        mv {{www_data_path}} {{www_data_path}}.bak 2>/dev/null
-        ln -sf {{data_path}}/www-data $LM_WWW_DATA_PATH
+        mv "{{www_data_path}}" "{{www_data_path}}.bak" 2>/dev/null
+        ln -sf "{{data_path}}/www-data" "$LM_WWW_DATA_PATH"
       fi
   fi
 else
