0439cee0c5b7296a629c486c229c8e7138faa0cf
Maarten van Gompel <proycon@anaproy.nl>
2022-01-17 22:59:30 +0100

    cleanup in lamachine-move-www-data

--- roles/lamachine-core/templates/lamachine-move-www-data.sh
+++ roles/lamachine-core/templates/lamachine-move-www-data.sh
@@ -3,11 +3,11 @@ if [ "{{www_data_path}}" != "{{lm_prefix}}/var/www-data" ]; then
   rm -rf "{{ lm_prefix }}/var/www-data.bak" >/dev/null 2>/dev/null
   if [ ! -d "{{www_data_path}}" ] && [ -d "{{ lm_prefix }}/var/www-data" ]; then
     echo "Moving www-data to external data volume... ({{data_path}}/www-data)">&2
-    mv "{{lm_prefix}}/var/www-data" "{{data_path}}/www-data" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    mv "{{lm_prefix}}/var/www-data" "{{www_data_path}}" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
     ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
   elif [ -d "{{www_data_path}}" ] && [ ! -L "{{ lm_prefix }}/var/www-data" ]; then
     echo "External data volume already contains www-data! Refusing to overwrite, just linking to existing data...">&2
-    mv "{{lm_prefix}}/www-data" "{{data_path}}/www-data" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    mv "{{lm_prefix}}/www-data" "{{www_data_path}}" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
     ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
   else
     echo "www-data already exists in on external volume and link was already established.. all ok">&2
