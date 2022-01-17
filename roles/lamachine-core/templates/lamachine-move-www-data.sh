7528f41f01557fd6b8ecf638116670ef8eaa0bec
Maarten van Gompel <proycon@anaproy.nl>
2022-01-17 22:28:05 +0100

    fixes for lamachine-move-www-data script

--- roles/lamachine-core/templates/lamachine-move-www-data.sh
+++ roles/lamachine-core/templates/lamachine-move-www-data.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-if [ -d "{{data_path}}" ]; then
-  rm -rf "{{ www_data_path }}.bak" >/dev/null 2>/dev/null
-  if [ "{{www_data_path}}" != "{{data_path}}/www-data" ]; then
-      if [ ! -e "{{data_path}}/www-data" ]; then
-        echo "Moving www-data to external data volume... ({{data_path}}/www-data)">&2
-        mv "{{www_data_path}}" "{{data_path}}/www-data" 2>/dev/null || mv "{{www_data_path}}" "{{www_data_path}}.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
-        ln -sf {{data_path}}/www-data {{www_data_path}}
-      else
-        echo "External data volume already contains www-data! Refusing to overwrite, just linking to existing data...">&2
-        mv "{{www_data_path}}" "{{www_data_path}}.bak" 2>/dev/null
-        ln -sf "{{data_path}}/www-data" "$LM_WWW_DATA_PATH"
-      fi
+if [ "{{www_data_path}}" != "{{lm_prefix}}/var/www-data" ]; then
+  rm -rf "{{ lm_prefix }}/var/www-data.bak" >/dev/null 2>/dev/null
+  if [ ! -d "{{www_data_path}}" ] && [ -d "{{ lm_prefix }}/var/www-data" ]; then
+    echo "Moving www-data to external data volume... ({{data_path}}/www-data)">&2
+    mv "{{lm_prefix}}/var/www-data" "{{data_path}}/www-data" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
+  elif [ -d "{{www_data_path}}" ] && [ ! -L "{{ lm_prefix }}/var/www-data" ]; then
+    echo "External data volume already contains www-data! Refusing to overwrite, just linking to existing data...">&2
+    mv "{{lm_prefix}}/www-data" "{{data_path}}/www-data" 2>/dev/null || mv "{{lm_prefix}}/var/www-data" "{{lm_prefix}}/var/www-data.bak" 2>/dev/null #in case move wasn't entirely succesful (due to permission denied error on removing)
+    ln -s "{{www_data_path}}" "{{lm_prefix}}/var/www-data"
+  else
+    echo "www-data already exists in on external volume and link was already established.. all ok">&2
   fi
-else
-  echo "Data path {{data_path}} does not exist. Not moving www-data anywhere.">&2
 fi
